4e1117b5a928a443c9675ae0b97a6124517a8a70
ITIvanov18 <ITIvanov18@codingburgas.bg>
2024-03-28 00:03:16 +0200

    Commit Pi approximation with alternating series

---
+++ Pi approximation/Pi approximation.cpp
@@ -0,0 +1,27 @@
+// Pi approximation.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double piVurhu4 = 0.0;
+    double znak = 1.0;
+
+    for (int i = 1; i <= 10000; i++) {
+        piVurhu4 = piVurhu4 + (znak / (2.0 * i - 1));
+        znak = znak * (-1);
+
+        if ((i + 1) % 100 == 0) {
+            double pi = piVurhu4 * 4;
+            cout << pi << endl;
+        }
+    }
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
