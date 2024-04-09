d2d32447ada87ed87517bfa73471c935b2a92010
ITIvanov18 <ITIvanov18@codingburgas.bg>
2024-04-10 01:54:29 +0300

    Commit Selection sort algorithm (with modification)

---
+++ Sorting an array in another array/Sorting an array in another array.cpp
@@ -0,0 +1,57 @@
+// Sorting an array in another array.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+const int arrLength = 6;
+
+int main()
+{
+	int arrayA[arrLength] = { 3, 11, 2, 9, 1, 5 };
+	int arrayB[arrLength];
+
+	int max = arrayA[0];
+	for (int i = 0; i < arrLength; i++) {
+		if (arrayA[i] > max) {
+			max = arrayA[i];
+		}
+	}
+	cout << "Max value: " << max << endl;
+
+	int min = arrayA[0];
+	for (int i = 0; i < arrLength; i++) {
+		if (arrayA[i] < min) {
+			min = arrayA[i];
+		}
+	}
+	cout << "Min value: " << min << endl;
+
+	for (int i = 0; i < arrLength; i++) {
+		int smallestElementIndex = 0;
+
+		// Откриваме кой е индексът на най-малкия елемент в arrayA
+		for (int j = 1; j < arrLength; j++) {
+			if (arrayA[j] < arrayA[smallestElementIndex]) {
+				smallestElementIndex = j;
+			}
+		}
+		// Поставяме най-малкия елемент в началото на arrayB
+		arrayB[i] = arrayA[smallestElementIndex];
+		// Заменяме стойността на най-малкия елемент в arrayA със стойността на най-големия
+		arrayA[smallestElementIndex] = max;
+	}
+
+	cout << "Sorted array: ";
+	for (int i : arrayB) {
+		cout << i << " ";
+	}
+	cout << endl;
+
+
+	return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
