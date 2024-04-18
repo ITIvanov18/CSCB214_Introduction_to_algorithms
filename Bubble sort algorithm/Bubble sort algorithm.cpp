3f142727aba993ea6e17617686a41b5add184eb5
ITIvanov18 <ITIvanov18@codingburgas.bg>
2024-04-18 23:44:09 +0300

    Commit bubble sort algorithm from lecture_16042024

---
+++ Bubble sort algorithm/Bubble sort algorithm.cpp
@@ -0,0 +1,50 @@
+// Bubble sort algorithm.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int size) {
+	bool swapped = true;
+
+	while (swapped) {
+		swapped = false;
+
+		for (int i = 0; i < size - 1; i++) {
+			if (arr[i] > arr[i + 1]) {
+				int temp = arr[i];
+				arr[i] = arr[i + 1];
+				arr[i + 1] = temp;
+				swapped = true;
+			}
+		}
+
+	}
+}
+
+void printArray(int arr[], int size) {
+	for (int i = 0; i < size; i++) {
+		cout << arr[i] << " ";
+	}
+}
+
+int main()
+{
+	int arr[] = {7, 3, 1, 4, 9, 15, 11};
+	int size = sizeof(arr) / sizeof(arr[0]);
+
+	cout << "Shuffled array: \n";
+	printArray(arr, size);
+
+	cout << "\n" << endl; 
+	bubbleSort(arr, size);
+
+	cout << "Sorted array: \n";
+	printArray(arr, size);
+
+
+	return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
